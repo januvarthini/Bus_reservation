4f5659b8f76401d356b7dd891957e8b51b065a77
januvarthini <sjanuvarthini@gmail.com>
2026-01-06 11:06:38 +0530

    Add bus_resv Java source files

---
+++ src/bus_resv/Bus_demo.java
@@ -0,0 +1,38 @@
+package bus_resv;
+
+import java.util.Scanner;
+
+import java.util.ArrayList;
+public class Bus_demo {
+
+	public static void main(String[] args) {
+		ArrayList<Bus> buses = new ArrayList<Bus>();
+		ArrayList<Bookings> bookings = new ArrayList<Bookings>();
+		
+		buses.add(new Bus(1,true,2));
+		buses.add(new Bus(2,false,50));
+		buses.add(new Bus(3,true,48));
+		
+		int userOpt = 1;
+		Scanner scanner = new Scanner(System.in);
+		
+		for(Bus b:buses) {
+			b.displayBusInfo();
+		}
+		
+		while(userOpt==1) {
+			System.out.println("Enter 1 to Book and 2 to exit");
+			userOpt = scanner.nextInt();
+			if(userOpt == 1) {
+				Bookings booking = new Bookings();
+				if(booking.isAvailable(bookings,buses)) {
+					bookings.add(booking);
+					System.out.println("Your booking is confirmed");
+				}
+				else
+					System.out.println("Sorry. Bus is full. Try another bus or date.");
+			}
+		}
+	}
+
+	}
